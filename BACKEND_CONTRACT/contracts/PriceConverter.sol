edcc4839c0841bffee703e3b7793a9fac302f38d
Carlos vouking <carlosvouking@gmail.com>
2022-07-10 01:34:34 +0200

    updating library PcCv

--- BACKEND_CONTRACT/contracts/PriceConverter.sol
+++ BACKEND_CONTRACT/contracts/PriceConverter.sol
@@ -17,7 +17,7 @@ library PriceConverter {
         // AggregatorV3Interface priceFeed = AggregatorV3Interface(
         //     0x8A753747A1Fa494EC906cE90E9f37563A8AF630e
         // );
-        (, int256 answer, , , ) = priceFeed.latestRoundData();
+        (, int256 answer, , , ) = priceFeed.latestRoundData(); //answer= priceOfETHIntermsofUSD
         // ETH/USD rate in 18 digit
         return uint256(answer * 10000000000);
     }
@@ -38,6 +38,7 @@ library PriceConverter {
     }
 
     // 1000000000
+    // Pass some ETH Amount and at the other side how much that Eth amount is worth in termq of USD 
     function getConversionRate(
         uint256 ethAmount,
         AggregatorV3Interface priceFeed
